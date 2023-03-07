c61ec7e5ce8c7bd08bc8ddec76fc1269c1cdee7d
James Lunt <jameslunt21@gmail.com>
2023-03-07 13:17:52 +0000

    Now running with properly distributed Train, Validate and Test sets

--- fine_tune.py
+++ fine_tune.py
@@ -5,9 +5,9 @@ import numpy as np
 from pathlib import Path
 from data_loader import MultiDataLoader 
 from train_copy import local_train,run_epoch
-#from train_copy import run_epoch
 from tqdm import tqdm
 import pandas as pd
+import warnings
 # Permanently changes the pandas settings
 pd.set_option('display.max_rows', None)
 pd.set_option('display.max_columns', None)
@@ -15,16 +15,15 @@ pd.set_option('display.width', None)
 pd.set_option('display.max_colwidth', -1)
 
 
-config_file = Path('config_personalisation.yaml')
-with open(config_file) as file:
-  config = yaml.safe_load(file)
+CONFIG_FILE = Path('CONFIG_personalisation.yaml')
+with open(CONFIG_FILE) as file:
+  CONFIG = yaml.safe_load(file)
 
-model_storage = config['model_storage']
-test_model = config['lco_model']
-fold = config['test_center_fold']
-
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-device = 'cuda'
+MODEL_STORAGE = CONFIG['model_storage']
+TEST_MODEL = CONFIG['lco_model']
+FOLD = CONFIG['validation_center_fold']
+DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+CENTER = CONFIG['data']['centres'][FOLD]
 
 
 #Start Model
@@ -35,29 +34,29 @@ def import_class(name):
 
 
 #Loads model weights from pretrained model
-def initialize_model(device, state_dict=None):
-    Model = import_class(config['model']['arch']['function'])
-    if config['model']['arch']['args']:
-        model_copy = Model(**config['model']['arch']['args'])
+def initialize_model(state_dict=None):
+    Model = import_class(CONFIG['model']['arch']['function'])
+    if CONFIG['model']['arch']['args']:
+        model_copy = Model(**CONFIG['model']['arch']['args'])
     else:
         model_copy = Model()
     if state_dict != None:
         model_copy.load_state_dict(state_dict)
     else:
         model_copy.load_state_dict(model.state_dict())
-    model_copy.to(device)
+    model_copy.to(DEVICE)
     return model_copy
 
 
 #Loads the pretrained the model or initialises a new one
 def load_model():
-    if config['train_private']: 
-        Model = import_class(config['model']['arch']['function'])
-        model = Model(**config['model']['arch']['args'])
+    if CONFIG['train_private']: 
+        Model = import_class(CONFIG['model']['arch']['function'])
+        model = Model(**CONFIG['model']['arch']['args'])
         print("Training without Global Model")
     else: 
-        checkpoint = torch.load(model_storage + test_model)['state_dict']
-        model = initialize_model(device, checkpoint)
+        checkpoint = torch.load(MODEL_STORAGE + TEST_MODEL)['state_dict']
+        model = initialize_model(checkpoint)
     return model
 
 
@@ -65,7 +64,7 @@ def load_model():
 def load_data():
     data = {} 
     fold_splits = []
-    for centre in config['data']['centres']:
+    for centre in CONFIG['data']['centres']:
         worker = centre 
         dl = MultiDataLoader([centre])
         data[centre]=(worker, dl)
@@ -79,11 +78,11 @@ def load_data():
 #Sets the criterion and optimiser for the model
 def set_crit_opt(model):
     #Criterion & Optimiser
-    Criterion = import_class(config['hyperparameters']['criterion']) #torch.nn.BCELoss
+    Criterion = import_class(CONFIG['hyperparameters']['criterion']) #torch.nn.BCELoss
     criterion = Criterion()
-    Opt = import_class(config['hyperparameters']['optimizer'])
-    learning_rate = float(config['hyperparameters']['lr'])
-    if config['model']['arch']['args']['early_layers_learning_rate']: # if zero then it's freeze
+    Opt = import_class(CONFIG['hyperparameters']['optimizer'])
+    learning_rate = float(CONFIG['hyperparameters']['lr'])
+    if CONFIG['model']['arch']['args']['early_layers_learning_rate']: # if zero then it's freeze
         # Low shallow learning rates instead of freezing
         low_lr_list = []
         high_lr_list = []
@@ -92,9 +91,9 @@ def set_crit_opt(model):
                 low_lr_list.append(param)
             else:
                 high_lr_list.append(param)
-        print(f"Initializing optimizer with learning rates {config['model']['arch']['args']['early_layers_learning_rate']} for the early and {learning_rate} for the final layers")
+        print(f"Initializing optimizer with learning rates {CONFIG['model']['arch']['args']['early_layers_learning_rate']} for the early and {learning_rate} for the final layers")
         opt = Opt([
-            {'params': low_lr_list, 'lr': float(config['model']['arch']['args']['early_layers_learning_rate'])},
+            {'params': low_lr_list, 'lr': float(CONFIG['model']['arch']['args']['early_layers_learning_rate'])},
             {'params': high_lr_list, 'lr': learning_rate}
         ], lr=learning_rate)
     else:
@@ -103,7 +102,7 @@ def set_crit_opt(model):
     return criterion, opt
 
 
-#Prints the result metrics every epoch adn updates early stop counter
+#Prints the result metrics every epoch aand updates early stop counter
 def log_results(epoch,train_loss,val_loss,val_accuracy,confusion_matrix, best_model_results,best_model,early_stop_counter):
     if train_loss != None: # Last epoch for federated is None
         print(f'''
@@ -122,7 +121,7 @@ def log_results(epoch,train_loss,val_loss,val_accuracy,confusion_matrix, best_mo
         best_model_results['train_loss'] = train_loss
         best_model_results['val_accuracy'] = val_accuracy
         best_model_results['confusion_matrix'] = confusion_matrix
-        best_model = initialize_model(device, model.state_dict())
+        best_model = initialize_model(model.state_dict())
     else:
         early_stop_counter+=1
     return best_model_results, best_model, early_stop_counter
@@ -137,45 +136,83 @@ def save_best_model(best_model, best_epoch, results_path):
 
 #Fine-tunes the model on a center
 def fine_tune_local_train(model,data,opt,criterion,fold_splits):
-    fold_splits = fold_splits[fold]
+    fold_splits = fold_splits[FOLD]
     test_index = fold_splits[1]
     test_set = {test_index: data[test_index]}
-    return local_train(test_set, model, opt, criterion, device, fold=fold, test=False)
+    return local_train(test_set, model, opt, criterion, DEVICE, fold=FOLD, test=False)
 
 
 #Tests model on a full center
 def test_on_center(data,model,opt,criterion,fold_splits):
-    fold_splits = fold_splits[fold]
+    fold_splits = fold_splits[FOLD]
     test_index = fold_splits[1]
     test_set = {test_index: data[test_index]}
-    return local_train(test_set, model, opt, criterion, device, fold=fold, test=True)
+    return local_train(test_set, model, opt, criterion, DEVICE, fold=FOLD, test=True)
+
+def fine_tune(model,data,opt,criterion,num_epochs):
+    dl = data[CENTER][1]
+
+    early_stop_counter, best_loss = 0, 10000
+    best_model_results = dict()
+    best_model_results['val_loss'] = best_loss
+    best_model = model
+    print("Testing")
+    training_loader, validation_loader, test_loader = dl.load(fold_index=FOLD)
+
+    #Accuracy to beat
+    #_, _, test_predictions, _, epoch_accuracies, test_confusion_matrix = run_epoch(FOLD,test_loader, model=best_model, opt=opt, criterion=criterion, device=DEVICE, is_training = False)
+    #test_accuracy_avg = np.mean(epoch_accuracies)
+    #print(test_accuracy_avg)
+    #print(test_confusion_matrix)
+    #print(test_predictions)
+
+    for epoch in range(0, num_epochs):
+        #Train
+        model, opt, _, epoch_losses, _, _ = run_epoch(FOLD,training_loader, model=model, opt=opt, criterion=criterion, device=DEVICE,is_training = True)
+        train_loss_avg = np.mean(epoch_losses)
+        #Validate
+        _, _, predictions, epoch_losses, epoch_accuracies, confusion_m = run_epoch(FOLD,validation_loader, model=model, opt=opt, criterion=criterion, device=DEVICE,is_training = False)
+        val_loss_avg = np.mean(epoch_losses)
+        val_accuracy_avg = np.mean(epoch_accuracies)
+        #Log
+        best_model_results,best_model,early_stop_counter = log_results(epoch,train_loss_avg,val_loss_avg,val_accuracy_avg,confusion_m, best_model_results,best_model,early_stop_counter)
+        if early_stop_counter == CONFIG['early_stop_checkpoint']: 
+            print("Reached early stop checkpoint")
+            break
+
+    print("Testing")
+    _, _, test_predictions, _, epoch_accuracies, test_confusion_matrix = run_epoch(FOLD,test_loader, model=best_model, opt=opt, criterion=criterion, device=DEVICE, is_training = False)
+    test_accuracy_avg = np.mean(epoch_accuracies)
+    print(test_accuracy_avg)
+    print(test_confusion_matrix)
+    print(test_predictions)
+            
+    return best_model, best_model_results
 
 
 if __name__=='__main__':
+    warnings.filterwarnings("ignore")  
+    warnings.simplefilter('ignore')
+
     model = load_model()
     data, fold_splits = load_data()
     criterion, opt = set_crit_opt(model)
     model_new = model
     opt_new = opt
-    num_epochs = config['num_epochs']
-    early_stop_counter, best_loss = 0, 10000
-    best_model_results = dict()
-    best_model_results['val_loss'] = best_loss
-    best_model = model
+    num_epochs = CONFIG['num_epochs']
 
+    best_model,best_model_results = fine_tune(model_new,data,opt,criterion,num_epochs)
     #_, _, _, test_predictions, _, _, test_accuracy_avg, test_confusion_matrix = test_on_center(data,model_new,opt_new,criterion,fold_splits) #Tests on a full center
-
-    for epoch in range(0,num_epochs):
+    save_best_model(best_model, best_model_results['epoch'],MODEL_STORAGE)
+""" for epoch in range(0,num_epochs):
         model_new,opt_new,_, _, train_loss, val_loss, val_accuracy, confusion_m = fine_tune_local_train(model_new,data,opt_new,criterion,fold_splits)
         model_new = model_new[0]
         opt_new = opt_new[0]
         best_model_results, best_model,early_stop_counter = log_results(epoch,train_loss,val_loss,val_accuracy,confusion_m,best_model_results,best_model,early_stop_counter)
         print(best_model_results)
-        if early_stop_counter == config['early_stop_checkpoint']: 
+        if early_stop_counter == CONFIG['early_stop_checkpoint']: 
             print("Reached early stop checkpoint")
             break
     print("The Best model is")
-    print(best_model_results)
-    #save_best_model(best_model, best_model_results['epoch'], config['output_model_storage'])
-        
-
+    print(best_model_results)"""
+    #save_best_model(best_model, best_model_results['epoch'], CONFIG['output_MODEL_STORAGE'])
